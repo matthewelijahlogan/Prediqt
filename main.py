5753963f84f80aa1da2c0b8a576ad14170f6273f
davincistouch <loganma@my.westmoreland.edu>
2025-06-12 01:07:04 -0400

    Fix CORS issues 2

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://prediqt.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
